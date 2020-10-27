025e2e4e09adcba9b6982262b9a2963933ad9556
em0 <em0@em0noiMac.local>
2020-10-27 15:16:06 +0900

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+// screens/home_screen.dart
+
+import 'package:flutter/material.dart';
+
+import '../widgets/sample_message.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SampleMessage(),
+    );
+  }
+}
---
+++ lib/widgets/sample_message.dart
@@ -0,0 +1,12 @@
+// widgets/sample_message.dart
+
+import 'package:flutter/material.dart';
+
+class SampleMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Hello, World"),
+    );
+  }
+}
